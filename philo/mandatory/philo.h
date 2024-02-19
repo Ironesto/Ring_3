ecba2e9dfa688ef14f85157f3d2a674d72d59a3a
ironesto <gabriel.p.g@hotmail.es>
2024-02-19 04:06:02 +0100

    .h

--- philo/mandatory/philo.h
+++ philo/mandatory/philo.h
@@ -9,39 +9,36 @@
 
 typedef struct s_forktb
 {
-    int				forktb;
-    pthread_mutex_t	mutex_forktb;
-}   t_forktb;
+	int				forktb;
+	pthread_mutex_t	mutex_forktb;
+}	t_forktb;
 
 typedef struct s_philo
 {
 	struct timeval	time;
 	int				philo;
-	time_t			ttotal;
-	time_t			*ms;
-	int	die;
-	int	slp;
-	int	eat;
-	time_t	td;
-	time_t		cont;
-	time_t		gone;
-	t_forktb	*fork;
-	t_forktb	*fork_l;
-	pthread_t	ph_thread;
+	time_t			tnow;
+	time_t			tdie;
+	time_t			final;
+	time_t			teat;
+	time_t			tslp;
+	time_t			neat;
+	t_forktb		*fork;
+	t_forktb		*fork_l;
+	pthread_t		ph_thread;
 }	t_philo;
 
-typedef struct	s_table
+typedef struct s_table
 {
-	struct timeval	time;
-	t_forktb	*forktb;
 	t_philo		*philo;
-	int	phl;
-	int	tdie;
-	int	teat;
-	int	tslp;
-	int	neat;
-	time_t		*tz;
-}	t_table;
+	t_forktb	*forktb;
+	int			phl;
+	time_t		tdie;
+	time_t		teat;
+	time_t		tslp;
+	time_t		neat;
+}   t_table;
+
 
 void	ft_init(t_table *table, char **argv, struct timeval	time);
 void	ft_initphilos(t_table *table);
